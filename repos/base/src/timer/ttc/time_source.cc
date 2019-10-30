d666fc7a04f004831151fc7f27f7981a3bbb58e7
Alexander Weidinger <alexwegoo@gmail.com>
2019-10-30 18:59:00 +0100

    Fix timer with correct clock calculation for Zybo

--- repos/base/src/timer/ttc/time_source.cc
+++ repos/base/src/timer/ttc/time_source.cc
@@ -9,97 +9,133 @@
 
 using namespace Genode;
 
+class debug : private Genode::Attached_mmio
+{
+private:
+  Genode::Env &_env;
+
+  struct ARM_CLK_CTRL : Register<0x120, 32> {
+    struct SRCSEL  : Bitfield<0, 2> { };
+    struct DIVISOR : Bitfield<8, 6> { };
+  };
+
+  struct ARM_PLL_CTRL : Register<0x100, 32> {
+    struct PLL_FDIV : Bitfield<12, 7> { };
+  };
+
+  struct SLCR_CLK_621_TRUE : Register<0x1C4, 32> {
+    struct CLK_621_TRUE : Bitfield<0, 1> { };
+  };
+
+public:
+  debug(Genode::Env &env) : Attached_mmio(env, 0xF8000000, 0xB78, false), _env(env) {
+    Genode::log("--- ARM_CLK_CTRL ---");
+    Genode::log("SRCSEL=", read<ARM_CLK_CTRL::SRCSEL>());
+    Genode::log("DIVISOR=", read<ARM_CLK_CTRL::DIVISOR>());
+    Genode::log("------- SLCR -------");
+    Genode::log("CLK_621_TRUE=", read<SLCR_CLK_621_TRUE::CLK_621_TRUE>());
+    Genode::log("--------------------");
+    Genode::log("--- ARM_PLL_CTRL ---");
+    Genode::log("PLL_FDIV=", read<ARM_PLL_CTRL::PLL_FDIV>());
+    Genode::log("--------------------");
+  }
+};
+
 void Timer::Time_source::schedule_timeout(Genode::Microseconds  duration,
                                           Timeout_handler      &handler)
 {
-	_handler = &handler;
+  _handler = &handler;
 
-	unsigned short const ticks = Genode::max((unsigned short)1, (duration.value * TICKS_PER_MS) / 1000);
-	_timer_irq.ack_irq();
-	_cleared_ticks = 0;
+  unsigned short const ticks = Genode::max((unsigned short)1, (duration.value * TICKS_PER_MS) / 1000);
+  _timer_irq.ack_irq();
+  _cleared_ticks = 0;
 
-	/* stop timer */
-	write<CNT_CNTRL::DIS>(1);
+  /* stop timer */
+  write<CNT_CNTRL::DIS>(1);
 
-	/* clear interrupts */
-	if (read<ISR>()) {
-		_timer_irq.ack_irq();
-	}
+  /* clear interrupts */
+  if (read<ISR>()) {
+    _timer_irq.ack_irq();
+  }
 
-	/* set new timeout */
-	write<MATCH_0>(COUNT_VALUE::MAX - ticks);
-	write<IER::IXR_MATCH_0>(1);
+  /* set new timeout */
+  write<MATCH_0>(COUNT_VALUE::MAX - ticks);
+  write<IER::IXR_MATCH_0>(1);
 
-	/* counter restart */
-	write<CNT_CNTRL::DIS>(0);
+  /* counter restart */
+  write<CNT_CNTRL::DIS>(0);
 }
 
 
 unsigned long timer_ticks_to_us(unsigned short const ticks, unsigned short const ticks_per_ms) {
-	enum {
-		HALF_WIDTH = (sizeof(unsigned short) << 2),
-		MSB_MASK  = ((unsigned short)~0) << HALF_WIDTH,
-		LSB_MASK  = ((unsigned short)~0) >> HALF_WIDTH,
-		MSB_RSHIFT = 6, // FIXME currently don't understand this value
-		LSB_LSHIFT = HALF_WIDTH - MSB_RSHIFT,
-	};
-	unsigned short const msb = ((((ticks & MSB_MASK) >> MSB_RSHIFT)
-	                             * 1000) / ticks_per_ms) << MSB_RSHIFT;
-	unsigned short const lsb = ((((ticks & LSB_MASK) << LSB_LSHIFT)
-	                             * 1000) / ticks_per_ms) >> LSB_LSHIFT;
-	return msb + lsb;
+  enum {
+	HALF_WIDTH = (sizeof(unsigned short) << 2),
+	MSB_MASK  = ((unsigned short)~0) << HALF_WIDTH,
+	LSB_MASK  = ((unsigned short)~0) >> HALF_WIDTH,
+	MSB_RSHIFT = 2, // FIXME currently don't understand this value
+	LSB_LSHIFT = HALF_WIDTH - MSB_RSHIFT,
+  };
+  unsigned short const msb = ((((ticks & MSB_MASK) >> MSB_RSHIFT)
+			       * 1000) / ticks_per_ms) << MSB_RSHIFT;
+  unsigned short const lsb = ((((ticks & LSB_MASK) << LSB_LSHIFT)
+			       * 1000) / ticks_per_ms) >> LSB_LSHIFT;
+  return msb + lsb;
 }
 
 
 Duration Timer::Time_source::curr_time()
 {
-	unsigned short const uncleared_ticks = COUNT_VALUE::MAX - read<COUNT_VALUE>() - _cleared_ticks;
-	unsigned long const uncleared_us     = timer_ticks_to_us(uncleared_ticks, (unsigned short)TICKS_PER_MS);
+  unsigned short const uncleared_ticks = COUNT_VALUE::MAX - read<COUNT_VALUE>() - _cleared_ticks;
+  unsigned long const uncleared_us     = timer_ticks_to_us(uncleared_ticks, (unsigned short)TICKS_PER_MS);
 
-	/* update time only on IRQs and if rate is under 1000 per second */
-	if (_irq || uncleared_us > 1000) {
-		_curr_time.add(Genode::Microseconds(uncleared_us));
-		_cleared_ticks += uncleared_ticks;
-	}
-	return _curr_time;
+  /* update time only on IRQs and if rate is under 1000 per second */
+  if (_irq || uncleared_us > 1000) {
+    _curr_time.add(Genode::Microseconds(uncleared_us));
+    _cleared_ticks += uncleared_ticks;
+  }
+  return _curr_time;
 }
 
 
 Genode::Microseconds Timer::Time_source::max_timeout() const
 {
-	static unsigned long max = timer_ticks_to_us((unsigned long)COUNT_VALUE::MAX, TICKS_PER_MS);
-	return Genode::Microseconds(max);
+  static unsigned long max = timer_ticks_to_us((unsigned long)COUNT_VALUE::MAX, TICKS_PER_MS);
+  return Genode::Microseconds(max);
 }
 
 
 void Timer::Time_source::_initialize()
 {
-	_timer_irq.sigh(_signal_handler);
+  _timer_irq.sigh(_signal_handler);
 
-	// disable ttc
-	write<CNT_CNTRL::DIS>(1);
+  debug debug(_env);
 
-	// select clock source and set prescaler value FIXME?
-	// default clk should be 133 MHz (QEMU) or 150 MHz
-	// count/2^(N+1); N = prescale value
-	write<CLK_CNTRL::SRC>(0);
-	write<CLK_CNTRL::PS_VAL>(0x7);
-	write<CLK_CNTRL::PS_EN>(1);
+  /* disable ttc */
+  write<CNT_CNTRL::DIS>(1);
 
-	// set interval value
-	write<INT_VAL>(COUNT_VALUE::MAX);
+  /* select clock source and prescaler value FIXME
+   * count/2^(N+1); N = prescale value */
+  write<CLK_CNTRL::SRC>(0);
+  /* the value of the prescaler should be 0x5 when the rest of the settings is unchanged
+   * currently somewhere there is a /2 factor missing in my clock calculations */
+  write<CLK_CNTRL::PS_VAL>(0x6);
+  /* enable prescaler */
+  write<CLK_CNTRL::PS_EN>(1);
 
-	// disable waveform
-	write<CNT_CNTRL::EN_WAVE>(1); // active low
-	// enable matching
-	write<CNT_CNTRL::MATCH>(1);
-	// set counting up
-	write<CNT_CNTRL::DECR>(0);
-	// disable interval mode
-	write<CNT_CNTRL::INT>(0);
-	// mask interrupts
-	write<IER>(0x0);
+  /* set interval value */
+  write<INT_VAL>(COUNT_VALUE::MAX);
 
-	// clear interrupts
-	read<ISR>();
+  /* disable waveform */
+  write<CNT_CNTRL::EN_WAVE>(1); // active low
+  /* enable matching */
+  write<CNT_CNTRL::MATCH>(1);
+  /* set counting up */
+  write<CNT_CNTRL::DECR>(0);
+  /* disable interval mode */
+  write<CNT_CNTRL::INT>(0);
+  /* mask interrupts */
+  write<IER>(0x0);
+
+  /* clear interrupts */
+  read<ISR>();
 }
--- repos/base/src/timer/ttc/time_source.h
+++ repos/base/src/timer/ttc/time_source.h
@@ -21,63 +21,73 @@ namespace Timer { class Time_source; }
 class Timer::Time_source : private Genode::Attached_mmio,
                            public  Genode::Signalled_time_source
 {
-	private:
+private:
 
-		enum { TICKS_PER_MS = 519 };
+  /*
+   * 50 MHz (PS_CLK) * 26 (PLL_FDIV) = 1300 MHz (ARM_PLL freq)
+   * 1300 MHz / 2 (DIVISOR) = 650 MHz (ARM clock)
+   * 650 MHz / 6 (since CLK_621==1) = 108 MHz (CPU_1x)
+   * 
+   * 108 MHz = 108.000 ticks/ms
+   * 108.000 / 2^(N+1) = 1688 (N = Prescale 0x5)
+   */
+  enum { TICKS_PER_MS = 1688 };
 
-		struct CNT_CNTRL : Register<0xc, 8> {
-			struct POL_WAVE : Bitfield<6, 1> { };
-			struct EN_WAVE  : Bitfield<5, 1> { };
-			struct RST      : Bitfield<4, 1> { };
-			struct MATCH    : Bitfield<3, 1> { };
-			struct DECR     : Bitfield<2, 1> { };
-			struct INT      : Bitfield<1, 1> { };
-			struct DIS      : Bitfield<0, 1> { };
-		};
+  struct CNT_CNTRL : Register<0xc, 8> {
+    struct DIS      : Bitfield<0, 1> { };
+    struct INT      : Bitfield<1, 1> { };
+    struct DECR     : Bitfield<2, 1> { };
+    struct MATCH    : Bitfield<3, 1> { };
+    struct RST      : Bitfield<4, 1> { };
+    struct EN_WAVE  : Bitfield<5, 1> { };
+    struct POL_WAVE : Bitfield<6, 1> { };
+  };
 
-		struct CLK_CNTRL : Register<0x0, 8> {
-			struct EXT_EDGE : Bitfield<6, 1> { };
-			struct SRC      : Bitfield<5, 1> { };
-			struct PS_VAL   : Bitfield<1, 4> { };
-			struct PS_EN    : Bitfield<0, 1> { };
-		};
+  struct CLK_CNTRL : Register<0x0, 8> {
+    struct PS_EN    : Bitfield<0, 1> { };
+    struct PS_VAL   : Bitfield<1, 4> { };
+    struct SRC      : Bitfield<5, 1> { };
+    struct EXT_EDGE : Bitfield<6, 1> { };
+  };
 
-		struct INT_VAL : Register<0x24, 16> { };
+  struct INT_VAL : Register<0x24, 16> { };
 
-		struct COUNT_VALUE : Register<0x18, 16> { enum { MAX = ~(access_t)0 }; };
+  struct COUNT_VALUE : Register<0x18, 16> { enum { MAX = ~(access_t)0 }; };
 
-		struct ISR : Register<0x54, 8> { };
+  struct ISR : Register<0x54, 8> { };
 
-		struct IER : Register<0x60, 8> {
-			struct Ev           : Bitfield<5, 1> { };
-			struct IXR_CNT_OVR  : Bitfield<4, 1> { };
-			struct IXR_MATCH_2  : Bitfield<3, 1> { };
-			struct IXR_MATCH_1  : Bitfield<2, 1> { };
-			struct IXR_MATCH_0  : Bitfield<1, 1> { };
-			struct IXR_INTERVAL : Bitfield<0, 1> { };
-		};
+  struct IER : Register<0x60, 8> {
+    struct IXR_INTERVAL : Bitfield<0, 1> { };
+    struct IXR_MATCH_0  : Bitfield<1, 1> { };
+    struct IXR_MATCH_1  : Bitfield<2, 1> { };
+    struct IXR_MATCH_2  : Bitfield<3, 1> { };
+    struct IXR_CNT_OVR  : Bitfield<4, 1> { };
+    struct Ev           : Bitfield<5, 1> { };
+  };
 
-		struct MATCH_0 : Register<0x30, 16> { };
+  struct MATCH_0 : Register<0x30, 16> { };
 
-		Genode::uint16_t _cleared_ticks { 0 };
+  Genode::uint16_t _cleared_ticks { 0 };
 
-		Genode::Irq_connection     _timer_irq;
-		Genode::Duration           _curr_time   { Genode::Microseconds(0) };
+  Genode::Irq_connection     _timer_irq;
+  Genode::Duration           _curr_time   { Genode::Microseconds(0) };
 
-		void _initialize();
+  void _initialize();
 
-	public:
+public:
 
-		Time_source(Genode::Env &env);
+  Genode::Env &_env;
+
+  Time_source(Genode::Env &env);
 
 
-		/*************************
-		 ** Genode::Time_source **
-		 *************************/
+  /*************************
+   ** Genode::Time_source **
+   *************************/
 
-		Genode::Duration curr_time() override;
-		void schedule_timeout(Genode::Microseconds duration, Timeout_handler &handler) override;
-		Genode::Microseconds max_timeout() const override;
+  Genode::Duration curr_time() override;
+  void schedule_timeout(Genode::Microseconds duration, Timeout_handler &handler) override;
+  Genode::Microseconds max_timeout() const override;
 };
 
 #endif /* _TIME_SOURCE_H_ */
